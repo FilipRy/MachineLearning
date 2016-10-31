8c3c4681b9d34115c9879cf59553b7a7d531b78b
Filip Rydzi <rydzi.filip@gmail.com>
2016-10-31 15:00:24 +0100

    image compression some comments removed

--- ImageCompression/computeCentroids.m
+++ ImageCompression/computeCentroids.m
@@ -13,21 +13,9 @@ function centroids = computeCentroids(X, idx, K)
 % Useful variables
 [m n] = size(X);
 
-% You need to return the following variables correctly.
 centroids = zeros(K, n);
 
 centr_usage_count = zeros(K);
-
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Go over every centroid and compute mean of all points that
-%               belong to it. Concretely, the row vector centroids(i, :)
-%               should contain the mean of the data points assigned to
-%               centroid i.
-%
-% Note: You can use a for-loop over the centroids to compute this.
-%
-
 
 for i=1:m
     u_c = idx(i);
